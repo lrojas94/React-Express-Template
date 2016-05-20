1048630090ad4e54587a487cc4397332ff292b8c
Luis Rojas <lrojas94@gmail.com>
2016-05-20 16:34:32 -0400

    Added bodyParser and a POST example

--- client/components/hello.jsx
+++ client/components/hello.jsx
@@ -4,13 +4,27 @@ import {Link} from 'react-router';
 
 export var Hello = React.createClass({
     displayName: 'Hello',
-    onClick: function(e){
+    sendGet: function(e){
     	e.preventDefault();
     	//Init ajax call:
     	$.ajax({
     		url: '/api',
     		method: 'GET',
     		dataType: 'json',
+        data: {msg: "Hello Server with Get!"},
+    		success: function(data){
+    			console.log(data);
+    		}
+    	});
+    },
+    sendPost: function(e){
+    	e.preventDefault();
+    	//Init ajax call:
+    	$.ajax({
+    		url: '/api',
+    		method: 'POST',
+    		dataType: 'json',
+        data: {msg: "Hello Server with Post!"},
     		success: function(data){
     			console.log(data);
     		}
@@ -19,12 +33,14 @@ export var Hello = React.createClass({
     render: function(){
         return (
           <div>
-			       <div>This is a react hello module</div>
-			       <br/>
-			       <input type='button' onClick={this.onClick} value='Get API Data'/>
-			       <br/>
-			       <Link to='/hello/helloAgainMessage'>Hello Again!!</Link>
-		       	   {this.props.children}
+			     <div>This is a react hello module</div>
+			     <br/>
+           <input type='button' onClick={this.sendGet} value='GET API Data'/>
+			     <br/>
+           <input type='button' onClick={this.sendPost} value='POST API Data'/>
+           <br/>
+           <Link to='/hello/helloAgainMessage'>Hello Again as Child of Hello</Link>
+		       {this.props.children}
 		      </div>
         );
     }
--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static('client/'));
 app.use(express.static('bower_components/'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/api',function(req,res){
+	console.log(req.query); //<- This is for a GET
+	res.send({"data" : "Working"});
+});
+
+app.post('/api',function(req,res){
+	console.log(req.body); //<- This is for a Post
 	res.send({"data" : "Working"});
 });
 
